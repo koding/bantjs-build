3de23e296579a28186edea36d9562884afccc388
tetsuo <ogunduz@gmail.com>
2015-02-11 20:11:35 +0200

    Release 0.1.4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var concat = require('concat-stream');
 var through = require('through2');
 var inherits = require('util').inherits;
 var normalize = require('bant-normalize');
+var flatten = require('bant-flatten');
 var Duplex = require('readable-stream/duplex');
 var Readable = require('readable-stream/readable');
 var browserify = require('browserify');
@@ -20,85 +21,106 @@ function build (b, opts) {
     b = browserify();
   }
 
-  if (!(this instanceof build)) return new build(b, opts);
+  var self = this;
 
-  Duplex.call(this, { objectMode: true });
+  if (!(self instanceof build)) return new build(b, opts);
+
+  Duplex.call(self, { objectMode: true });
 
   if (!opts) opts = {};
 
+  self._piping = false;
+  self._b = b;
+  self._normalize = normalize(opts);
+  self._branches = [];
+  self._nodes = [];
+  self._globals = opts.globals || {};
+  self._watch = ('function' === typeof b.close);
+
+  self._normalize.on('data', function (branch) {
+    self._branches.push(branch);
+  });
+
+  self._normalize.on('end', function () {
+    self._nodes = flatten(self._branches);
+    self._end();
+  });
+
+  self.once('finish', function () { self._normalize.end(); });
+}
+
+build.prototype._wrap = function (branch) {
+  var src = '';
+  if ('object' === typeof branch.globals) {
+    src += "var globals = require('globals');\n";
+    Object.keys(branch.globals).forEach(function (key) {
+      src += "globals['" + key + "'] = "
+          + JSON.stringify(branch.globals[key]) + ";\n";
+    });
+  }
+
+  // todo: investigate dedupe fail, we don't want to expose fullpaths
+  src += "module.exports = require('" + branch.main + "');\n";
+
+  return read(src);
+};
+
+build.prototype._wrapGlobals = function () {
+  var self = this, 
+      src = 'var g = ' 
+        + JSON.stringify(self._globals) 
+        + ';\nmodule.exports = g;';
+  return read(src);
+};
+
+build.prototype._end = function () {
   var self = this,
-      rows = [],
-      watch = ('function' === typeof b.close);
-      globals = opts.globals || {};
+      branches = self._branches,
+      nodes = self._nodes,
+      b = this._b,
+      outputs = [];
 
-  this._piping = false;
-  this._b = b;
+  nodes.forEach(function (node) {
+    b.exclude(node.expose);
+  });
 
-  (this._normalize = normalize(opts))
-    .on('data', function (row) { rows.push(row); })
-
-    .once('end', function () {
-
-      var g = extend({}, globals),
-          outputs = [];
-
-      rows.forEach(function (row) {
-        if (row.name) {
-          var src = '';
-          if ('object' === typeof row.globals) {
-            src += "var globals = require('globals');\n";
-            Object.keys(row.globals).forEach(function (key) {
-              src += "globals['" + key + "'] = "
-                  + JSON.stringify(row.globals[key]) + ";\n";
-            });
-          }
-
-          // XXX: dedupe fail
-          //src += "module.exports=require('./" + path.basename(row.main.file) + "');\n";
-
-          src += "module.exports=require('" + row.main.file + "');\n";
-
-          b.require(read(src), {
-            entry: true,
-            expose: row.main.expose,
-            basedir: path.dirname(row.main.file)
-          }).exclude(row.main.expose);
-
-          if (!watch)
-            outputs.push(self._packup(row.name));
-          else {
-            outputs.push(function () {
-              return self._packup(row.name);
-            });
-          }
-        }
-      });
-
-      g = 'var g = ' + JSON.stringify(g) + ';\nmodule.exports = g;';
-
-      b.require(read(g), { 
-        entry: true,
-        expose: 'globals'
-      }).exclude('globals').plugin(factor, { outputs: outputs });
-
-      var s = null;
-      if (!watch) {
-        s = b.bundle(function (err, src) {
-          if (err) throw err;
-        }).pipe(self._packup());
-      } else {
-        b.on('update', function (ids) {
-          debug('updated ', ids);
-          self._wbundle();
-        });
-        s = self._wbundle().pipe(function () {
-          return self._packup();
-        }());
-      }
+  branches.forEach(function (branch) {
+    b.require(self._wrap(branch), {
+      entry: true,
+      expose: branch.expose,
+      basedir: path.dirname(branch.main)
     });
 
-  this.once('finish', function () { self._normalize.end(); });
-}
+    if (!self._watch) {
+      outputs.push(self._packup(branch.name));
+    } else {
+      outputs.push(self._packup.bind(self, branch.name));
+    }
+  });
+
+  b
+    .require(self._wrapGlobals(), {
+      entry: true,
+      expose: 'globals'
+    })
+    .exclude('globals')
+    .plugin(factor, { outputs: outputs });
+
+  if (!self._watch) {
+    s = b.bundle(function (err, src) {
+      if (err) throw err;
+    }).pipe(self._packup());
+  } else {
+    b.on('update', function (ids) {
+      debug('updated ', ids);
+      self._wbundle();
+    });
+    s = self._wbundle().pipe(function () {
+      return self._packup();
+    }());
+  }
+
+};
 
 build.prototype.pipe = function () {
   this._buf = [];
